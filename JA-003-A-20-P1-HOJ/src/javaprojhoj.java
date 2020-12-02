a0cb9d0d22468c266e9296625f52fb7455c31a0d
RDMcLeod <mcleod.r.derrick@gmail.com>
2020-12-02 17:49:47 -0500

    initial commit

---
+++ JA-003-A-20-P1-HOJ/src/javaprojhoj.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class javaprojhoj {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+				
+		//Scanner in = new Scanner(System.in);
+		//String s = in.nextLine();
+		int x;
+		x = 5;
+		double d = 5.5;
+		
+		System.out.println(d);
+	}
+
+}
+
