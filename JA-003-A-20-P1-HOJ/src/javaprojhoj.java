43224648daf38968d2cd923f15b2689f8d9f0832
RDMcLeod <mcleod.r.derrick@gmail.com>
2020-12-03 21:13:19 -0500

    update

--- JA-003-A-20-P1-HOJ/src/javaprojhoj.java
+++ JA-003-A-20-P1-HOJ/src/javaprojhoj.java
@@ -3,14 +3,19 @@ import java.util.Scanner;
 public class javaprojhoj {
 
 	public static void main(String[] args) {
-		System.out.println("derrick");
+		System.out.println("enter your name");
 				
 		Scanner in = new Scanner(System.in);
 		String s = in.nextLine();
 		
 		if(s.equals("derrick")) {
 		   System.out.println("Hey derrick");
-		} else
+		} else if(s.equals("Louis")) {
+			System.out.println("Hey Louis");
+		} else {
+			System.out.println("your not welcome here");
+				
+		}
 			
 		in.close();
 			
