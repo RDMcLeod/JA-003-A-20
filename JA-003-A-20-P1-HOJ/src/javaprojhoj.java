a3df3f7e3362658f36e8c6930fde77693e50fa24
RDMcLeod <mcleod.r.derrick@gmail.com>
2020-12-04 21:52:43 -0500

    Update javaprojhoj.java

--- JA-003-A-20-P1-HOJ/src/javaprojhoj.java
+++ JA-003-A-20-P1-HOJ/src/javaprojhoj.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class javaprojhoj {
 
 	public static void main(String[] args) {
-		System.out.println("enter your name");
-				
+		
 				Scanner in = new Scanner(System.in);
+				System.out.println("Give us a size");
 				int size = in.nextInt();
 				
 				
