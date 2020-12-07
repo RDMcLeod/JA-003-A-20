e5f4a1164a2dc6ed7e90b4ff46877e0c025f2bb9
RDMcLeod <mcleod.r.derrick@gmail.com>
2020-12-06 20:52:26 -0500

    List & Array rework
    Using ist & Array List

--- JA-003-A-20-P1-HOJ/src/javaprojhoj.java
+++ JA-003-A-20-P1-HOJ/src/javaprojhoj.java
@@ -1,23 +1,32 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+
+import javax.print.DocFlavor.STRING;
 
 public class javaprojhoj {
 
 	public static void main(String[] args) throws FileNotFoundException {
 		
-		Scanner in = new Scanner(new File("Untitled 4.txt"));
-				
-		while(in.hasNextLine()) {
-			System.out.println(in.nextLine());
-					
-				}
+		Scanner in = new Scanner(new File("students.txt"));
+		
+		List<String> students = new ArrayList<String>();
+		
+			while(in.hasNextLine()) {
+			    students.add(in.nextLine());
+	    }
+			
+		for(int i = 0; i < students.size(); i++) {
+			System.out.println("name:" + students.get (i));
+		}
 				
 		in.close();
 							
-			}
+	}
 						
-		}
+}
 		
 		
 					
