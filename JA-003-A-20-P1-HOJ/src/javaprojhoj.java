10ba0784ebdd28e1eb042280629074af2a9eb761
RDMcLeod <mcleod.r.derrick@gmail.com>
2020-12-10 22:40:15 -0500

    Iterators
    Iterators

--- JA-003-A-20-P1-HOJ/src/javaprojhoj.java
+++ JA-003-A-20-P1-HOJ/src/javaprojhoj.java
@@ -1,31 +1,32 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.Scanner;
 
 public class javaprojhoj {
 
 	public static void main(String[] args)  {
-				
-		
 		LinkedList<String> names = new LinkedList<String>();
 		names.push("russ");
 		names.push("Jane");
 		names.push("steve");
 		
-		System.out.println(names.pop());
-		System.out.println(names.removeFirst());
-		System.out.println(names.remove());
-	}
-
-	
+		ListIterator<String> it = names.listIterator();
+		it.next();
+		it.next();
+		it.add("fred");
+		
+			for(String s: names) {
+			System.out.println(s);
 	}
 						
-
+  }
 		
-		
+}		
 					
 			
 		
