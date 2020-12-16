6756fae97394e4db10cf50d169359f78b4553468
RDMcLeod <mcleod.r.derrick@gmail.com>
2020-12-15 20:04:29 -0500

    labyrinthe deuxième partie / Working But terminated
    labyrinthe deuxième partie /  Working But terminated

--- JA-003-A-20-P1-HOJ/src/javaprojhoj.java
+++ JA-003-A-20-P1-HOJ/src/javaprojhoj.java
@@ -17,10 +17,25 @@ public class javaprojhoj {
 			Position p = new Position(3, 0);
 			path.push(p);
 			
-			maze[path.peek().y] [path.peek().x]= 0; 
-	}		
+			int y = path.peek().y;
+			int x = path.peek().x;
+			
+			maze[y] [x]= 0; 
+			
+			while (true) {
+			//down
+			if(maze [y+1][x]==2) {
+				System.out.println("You won!");
+				return;
+			}else if (maze [y+1][x]==1) {
+				System.out.println("Moved down");
+				path.push(new Position(y+1, x));
+				continue;
+			}
+		}		
 					
-	}			
+		}
+}
 
 			
 	
