470baad3e028e60a60f6a07b83442e0e96e920b9
RDMcLeod <mcleod.r.derrick@gmail.com>
2020-12-15 20:32:01 -0500

    Part Deux Labarinthe - Still Building P2 D10
    Part Deux Labarinthe - Still Building P2 D10

--- JA-003-A-20-P1-HOJ/src/javaprojhoj.java
+++ JA-003-A-20-P1-HOJ/src/javaprojhoj.java
@@ -4,14 +4,14 @@ public class javaprojhoj {
 
 		static int [][] maze = {
 			{2,0,0,1},
-			{1,1,0,1},
+			{1,1,2,0},
 			{0,1,1,1}
 	};
 		//0 = wall
 		//1 = path
 		//2 = destination
 		
-		static LinkedList<Position> path = new LinkedList<Position>();
+		static LinkedList<Position> path = new LinkedList<Position>(); 
 			
 		public static void main(String[] args)  {
 			Position p = new Position(3, 0);
@@ -32,12 +32,36 @@ public class javaprojhoj {
 				path.push(new Position(y+1, x));
 				continue;
 			}
-		}		
-					
+			//Left
+			if(maze [x-1][x]==2) {
+				System.out.println("You won!");
+				return;
+			}else if (maze [y][x-1]==1) {
+				System.out.println("Moved down");
+				path.push(new Position(y, x-1));
+				continue;
+		    }
+			//Up
+			if(maze [y-1][x]==2) {
+				System.out.println("You won!");
+				return;
+			}else if (maze [y-1][x]==1) {
+				System.out.println("Moved down");
+				path.push(new Position(y-1, x));
+				continue;
+		    }
+			//Right
+			if(maze [y][x+1]==2) {
+				System.out.println("You won!");
+				return;
+			}else if (maze [y][x+1]==1) {
+				System.out.println("Moved down");
+				path.push(new Position(y, x+1));
+				continue;
+            }
+			}
 		}
-}
-
-			
+}	
 	
 
 
