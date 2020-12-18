44edf9bba813fb1c410889c3866d107a37e9d667
RDMcLeod <mcleod.r.derrick@gmail.com>
2020-12-18 12:12:15 -0500

    Update javaprojhoj.java

--- JA-003-A-20-P1-HOJ/src/javaprojhoj.java
+++ JA-003-A-20-P1-HOJ/src/javaprojhoj.java
@@ -2,70 +2,63 @@ import java.util.LinkedList;
 
 public class javaprojhoj {
 
-		static int [][] maze = {
-			{2,0,1,1},
-			{1,1,2,0},
-			{0,1,1,1}
-	};
-		//0 = wall
-		//1 = path
-		//2 = destination
-		
-		static LinkedList<Position> path = new LinkedList<Position>(); 
-			
-		public static void main(String[] args)  {
-			Position p = new Position(0, 3);
-			path.push(p);
-			
-			int y = path.peek().y;
-			int x = path.peek().x;
-			
-			maze[y] [x]= 0; 
-			
-			while (true) {
-			//down
-			if(maze[y+1][x]==2) {
+	static int[][] maze = { { 2, 0, 1, 1 }, { 1, 1, 2, 0 }, { 0, 1, 1, 1 } };
+	// 0 = wall
+	// 1 = path
+	// 2 = destination
+
+	static LinkedList<Position> path = new LinkedList<Position>();
+
+	public static void main(String[] args) {
+		Position p = new Position(0, 3);
+		path.push(p);
+
+		int y = path.peek().y;
+		int x = path.peek().x;
+
+		maze[y][x] = 0;
+
+		while (true) {
+			// down
+			if (maze[y + 1][x] == 2) {
 				System.out.println("Moved down. You won!");
 				return;
-			}else if(maze[y+1][x]==1) {
+			} else if (maze[y + 1][x] == 1) {
 				System.out.println("Moved down");
-				path.push(new Position(y+1, x));
+				path.push(new Position(y + 1, x));
 				continue;
 			}
-			//Left
-			if(maze[y][x-1]==2) {
-				System.out.println("Moved left. You won!");
+			// Left
+			if (maze[y][x - 1] == 2) {
+				System.out.println("Moved left.0 You won!");
 				return;
-			}else if(maze[y][x-1]==1) {
+			} else if (maze[y][x - 1] == 1) {
 				System.out.println("Moved left");
-				path.push(new Position(y, x-1));
+				path.push(new Position(y, x - 1));
 				continue;
-		    }
-			//Up
-			if(maze[y-1][x]==2) {
+			}
+			// Up
+			if (maze[y - 1][x] == 2) {
 				System.out.println("You won!");
 				return;
-			}else if(maze [y-1][x]==1) {
+			} else if (maze[y - 1][x] == 1) {
 				System.out.println("Moved up. You won!");
-				path.push(new Position(y-1, x));
+				path.push(new Position(y - 1, x));
 				continue;
-		    }
-			//Right
-			if(maze[y][x+1]==2) {
+			}
+			// Right
+			if (maze[y][x + 1] == 2) {
 				System.out.println("Moved right. You won!");
 				return;
-			}else if(maze [y][x+1]==1) {
+			} else if (maze[y][x + 1] == 1) {
 				System.out.println("Moved right");
-				path.push(new Position(y,x+1));
+				path.push(new Position(y, x + 1));
 				continue;
-            }
+			}
 			path.pop();
-			if(path.size()< 0) {
+			if (path.size() < 0) {
 				System.out.println("No path");
 			}
 		}
 	}
 }
-	
-
-
